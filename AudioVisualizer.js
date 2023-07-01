53ca9e86c6294bddd9b4a3fb9f6011848dd9200a
Karthik0055 <110559433+Karthik0055@users.noreply.github.com>
2023-07-01 12:57:54 +0530

    Update AudioVisualizer.js

--- AudioVisualizer.js
+++ AudioVisualizer.js
@@ -1,37 +1,31 @@
-import React, { useEffect } from 'react';
-
-function AudioVisualizer() {
-  useEffect(() => {
-    // Code related to audio visualization
+    // Get access to the microphone
     navigator.mediaDevices.getUserMedia({ audio: true })
       .then(function(stream) {
         var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        console.log(audioContext);
+       console.log(audioContext);
         var audioSource = audioContext.createMediaStreamSource(stream);
         var analyser = audioContext.createAnalyser();
-        analyser.fftSize = 1024;
+        analyser.fftSize = 2048;
         var bufferLength = analyser.frequencyBinCount;
         var dataArray = new Uint8Array(bufferLength);
-
         audioSource.connect(analyser);
-
         function updateVisualizer() {
           analyser.getByteFrequencyData(dataArray);
-
           var audioBars = document.getElementsByClassName('audio-bar');
-          var audio = document.querySelectorAll('.audio');
-          var audio1 = document.querySelectorAll('.audio1');
-
-          var scaleFactor = 1; // Adjust this value to control the height change sensitivity
-
-          for (var i = 0; i < audioBars.length; i++) {
+          var scaleFactor = 2; // Adjust this value to control the height change sensitivity
+          var first = document.getElementsByClassName( "first" );
+          var second = document.getElementsByClassName( "second" );
+          var middle = document.getElementsByClassName( "middle" );
+          var third = document.getElementsByClassName( "third" );
+          var last = document.getElementsByClassName( "last" );
+          for (var i = 0; i < middle.length; i++) {
             var barHeight = dataArray[i] / scaleFactor;
-            audioBars[i].style.height = barHeight/9 + 'px';
-            audio[i].style.height = barHeight/18 +'px';
-            audio1[i].style.height = barHeight/18 +'px';
-            
+            first[i].style.height = barHeight/6 + 'px';
+            second[i].style.height = barHeight/3 + 'px';
+            middle[i].style.height = barHeight/2 + 'px';
+            third[i].style.height = barHeight/3 + 'px';
+            last[i].style.height = barHeight/6 + 'px';
           }
-
           requestAnimationFrame(updateVisualizer);
         }
 
@@ -40,17 +34,3 @@ function AudioVisualizer() {
       .catch(function(err) {
         console.error(err);
       });
-  }, []); // Empty dependency array ensures the effect runs only once
-  
-  return (
-    <div className='d'>
-      <section>    
-   <div class="audio reflection blue"></div>
-   <div class="audio-bar reflection yellow"></div> 
-   <div class="audio audio1 reflection green"></div> 
- </section>
-    </div>
-  );
-}
-
-export default AudioVisualizer;
